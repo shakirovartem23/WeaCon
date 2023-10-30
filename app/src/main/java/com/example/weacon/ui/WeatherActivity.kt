06b6ee9d9d5d65f6dcfe782d7e00151608b059bf
shakirovartrem23 <artem.shakirov23@mail.ru>
2023-10-30 20:57:19 +0300

    Windows adds

--- app/src/main/java/com/example/weacon/ui/WeatherActivity.kt
+++ app/src/main/java/com/example/weacon/ui/WeatherActivity.kt
@@ -24,10 +24,45 @@ class WeatherActivity : AppCompatActivity() {
         setContentView(R.layout.activity_weather)
 
         val pager = findViewById<ViewPager2>(R.id.viewPager)
-        pager.adapter = ViewPageAdapter(listOf(R.drawable._db5b2ce7ba03746b6b68d210941ddbd, R.drawable.free_icon_minus_sign_of_a_line_in_horizontal_position_32320))
+        pager.adapter = ViewPageAdapter(
+            listOf(
+                R.drawable._1,
+                R.drawable._2,
+                R.drawable._3,
+                R.drawable._4,
+                R.drawable. _5,
+                R.drawable._6,
+                R.drawable._7,
+                R.drawable._8,
+                R.drawable._9,
+                R.drawable._10,
+                R.drawable._11,
+                R.drawable._12,
+                R.drawable._13,
+                R.drawable._14,
+                R.drawable._15,
+                R.drawable._16,
+                R.drawable._17,
+                R.drawable._18,
+                R.drawable._19,
+                R.drawable._20,
+                R.drawable._21,
+                R.drawable._22,
+                R.drawable._23,
+                R.drawable._24,
+                R.drawable._25,
+                R.drawable._26,
+                R.drawable._28,
+                R.drawable._29,
+                R.drawable._30,
+                R.drawable._31,
+                R.drawable._32
+        )
+        )
 
         val dots = findViewById<DotsIndicator>(R.id.dots)
         dots.attachTo(pager)
+
 
         //StatusBar transparent
         window.clearFlags(
