1f8d43f6df56da53c968049c9a73d016870aa45e
Artem <artem.shakirov23@mail.ru>
2023-11-01 13:40:37 +0300

    Mac adds

--- app/src/main/java/com/example/weacon/ui/WeatherActivity.kt
+++ app/src/main/java/com/example/weacon/ui/WeatherActivity.kt
@@ -1,6 +1,8 @@
 package com.example.weacon.ui
 
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Half.toFloat
 import android.util.Log
 import android.view.View
 import android.view.WindowManager
@@ -10,6 +12,23 @@ import androidx.viewpager2.widget.ViewPager2
 import com.example.weacon.R
 import com.example.weacon.Request.loadWeather
 import com.example.weacon.viewPager.ViewPageAdapter
+import com.github.mikephil.charting.charts.CombinedChart
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.AxisBase
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.components.YAxis
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.data.CombinedData
+import com.github.mikephil.charting.data.DataSet
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.formatter.IAxisValueFormatter
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
+import com.github.mikephil.charting.formatter.ValueFormatter
 import com.tbuonomo.viewpagerdotsindicator.DotsIndicator
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
@@ -110,6 +129,9 @@ class WeatherActivity : AppCompatActivity() {
             val textCloudEvening = findViewById<TextView>(R.id.textCloudEvening)
 
             if(weather!=null){
+
+                Log.d(TAG, weather.forecasts.size.toString())
+
                 //Other
                 textC.text = weather.fact.temp.toString()
                 textFeels.text = weather.fact.feels_Like.toString()
@@ -137,6 +159,81 @@ class WeatherActivity : AppCompatActivity() {
                 textWindEvening.text = weather.forecasts.first().parts.evening.wind_speed.toInt().toString()
                 textPaEvening.text = weather.forecasts.first().parts.evening.pressure_pa.toString()
                 textCloudEvening.text = weather.forecasts.first().parts.evening.cloudness.toInt().toString()
+
+                val chart = findViewById<LineChart>(R.id.chart)
+                val axisLeft = chart.axisLeft
+                val axisRight = chart.axisRight
+                val xAxis = chart.xAxis
+                val entries = mutableListOf<Entry>()
+                val entries1 = mutableListOf<Entry>()
+                val entries2 = mutableListOf<Entry>()
+
+                var count = 0
+
+                for (value in weather.forecasts){
+                    count+=1
+                    entries+=Entry(count.toFloat(), value.parts.day.temp_avg.toFloat())
+                    entries1+=Entry(count.toFloat(), value.parts.day.pressure_pa.toFloat()-1000)
+                    entries2+=Entry(count.toFloat(), value.parts.day.wind_speed.toFloat())
+                }
+
+                val chartDataSet = LineDataSet(entries, "Temperature, t°C")
+                chartDataSet.axisDependency = YAxis.AxisDependency.LEFT
+                chartDataSet.color = Color.BLACK
+                chartDataSet.gradientColor
+                chartDataSet.circleColors = listOf(Color.LTGRAY)
+                chartDataSet.circleRadius = 5F
+                chartDataSet.lineWidth = 4F
+                chartDataSet.setDrawValues(false)
+                chartDataSet.mode = LineDataSet.Mode.CUBIC_BEZIER
+                chartDataSet.cubicIntensity = 0.2F
+
+                val chartDataSet1 = LineDataSet(entries1, "Pressure, Pa-1000", )
+                chartDataSet1.axisDependency = YAxis.AxisDependency.LEFT
+                chartDataSet1.color = Color.RED
+                chartDataSet1.circleColors = listOf(Color.GREEN)
+                chartDataSet1.circleRadius = 5F
+                chartDataSet1.lineWidth = 4F
+                chartDataSet1.setDrawValues(false)
+                chartDataSet1.mode = LineDataSet.Mode.CUBIC_BEZIER
+                chartDataSet1.cubicIntensity = 0.2F
+
+                val chartDataSet2 = LineDataSet(entries2, "Wind speed, m/s")
+                chartDataSet2.axisDependency = YAxis.AxisDependency.LEFT
+                chartDataSet2.color = Color.BLUE
+                chartDataSet2.circleColors = listOf(Color.YELLOW)
+                chartDataSet2.circleRadius = 5F
+                chartDataSet2.lineWidth = 4F
+                chartDataSet2.setDrawValues(false)
+                chartDataSet2.mode = LineDataSet.Mode.CUBIC_BEZIER
+                chartDataSet2.cubicIntensity = 0.2F
+
+
+                val data = LineData(listOf(chartDataSet, chartDataSet1, chartDataSet2))
+                chart.data = data
+
+                chart.axisRight.setDrawAxisLine(false)
+                axisLeft.isEnabled = false
+                axisLeft.setDrawAxisLine(false)
+                axisRight.setDrawAxisLine(false)
+                axisRight.isEnabled = false
+                xAxis.isEnabled = false
+                xAxis.position = XAxis.XAxisPosition.BOTTOM
+                xAxis.granularity = 1F
+                xAxis.labelCount = 0
+                chart.setTouchEnabled(false)
+                chart.legend.isEnabled = false
+                xAxis.setDrawGridLines(false)
+                xAxis.setDrawAxisLine(false)
+                chart.isScaleXEnabled = true
+                chart.description = null
+
+                val legend = chart.legend
+                legend.form = Legend.LegendForm.CIRCLE
+                legend.isEnabled = true
+
+                chart.animateXY(8000, 10000)
+                chart.invalidate()
             }
         }
 
